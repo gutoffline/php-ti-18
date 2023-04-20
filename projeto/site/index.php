b18895ad58d03310c7c083c9ea79e500240812a5
Guto Xavier <gutoffline@gmail.com>
2023-04-20 20:06:14 -0300

    banner

--- projeto/site/index.php
+++ projeto/site/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>iGames</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css">
 </head>
 
 <body>
@@ -19,19 +20,46 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
+                    </li> -->
                 </ul>
                 <form action="login.php" method="post" class="d-flex">
-                    <input class="form-control me-2" type="email" placeholder="E-mail">
-                    <input class="form-control me-2" type="password" placeholder="Senha">
-                    <button class="btn btn-outline-success" type="submit">Entrar</button>
-                    <button class="btn btn-outline-success" type="submit">Cadastrar</button>
+                    <input class="form-control me-2" type="email" name="email" placeholder="E-mail">
+                    <input class="form-control me-2" type="password" name="senha" placeholder="Senha">
+                    <button class="btn btn-outline-success bt-roxo me-2" type="submit">Entrar</button>
+
+                    <a href="novo-usuario.php" class="btn btn-outline-primary">Cadastrar</a>
                 </form>
             </div>
         </div>
     </nav>
+
+    <div class="container-fluid banner">
+        <div id="carouselExample" class="carousel slide carousel-fade">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/banner01.jpg" class="d-block w-100" alt="The Last Of US Part II">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/banner02.jpg" class="d-block w-100" alt="hogwarts legacy">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/banner03.jpg" class="d-block w-100" alt="Horizon Forbidden West">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
