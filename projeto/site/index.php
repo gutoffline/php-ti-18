a2a1c7a3ccea95c9d17be9352a5c1c59d98217a6
Guto Xavier <gutoffline@gmail.com>
2023-04-20 22:12:11 -0300

    formulario de contato

--- projeto/site/index.php
+++ projeto/site/index.php
@@ -29,38 +29,56 @@
 
     <div class="container mt-5 mb-3">
         <div class="row">
-            <div class="col-12 text-center"><h1>Jogos em destaque</h1></div>
-            <div class="col-12 text-center"><p>Aqui você encontrará os jogos mais incríveis e envolventes do momento, cuidadosamente selecionados para a sua diversão. Desafie suas habilidades, experimente novos mundos e personagens, e divirta-se como nunca antes. Com gráficos de tirar o fôlego e jogabilidade viciante, nossa seleção de jogos em destaque é perfeita para os gamers mais exigentes. Venha conferir e descubra o seu próximo jogo favorito!</p></div>
+            <div class="col-12 text-center">
+                <h1>Jogos em destaque</h1>
+            </div>
+            <div class="col-12 text-center">
+                <p>Aqui você encontrará os jogos mais incríveis e envolventes do momento, cuidadosamente selecionados para a sua diversão. Desafie suas habilidades, experimente novos mundos e personagens, e divirta-se como nunca antes. Com gráficos de tirar o fôlego e jogabilidade viciante, nossa seleção de jogos em destaque é perfeita para os gamers mais exigentes. Venha conferir e descubra o seu próximo jogo favorito!</p>
+            </div>
         </div>
         <div class="row">
-            <div class="col-3 text-center">
-                <img src="img/jogo01.jpg" class="img-fluid">
-                <h5 class="mt-3 mb-3">Nome do Jogo</h5>
-                <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
+            <?php
+            for ($i = 0; $i < 8; $i++) :
+            ?>
+                <div class="col-md-3 text-center mb-4">
+                    <img src="img/jogo01.jpg" class="img-fluid">
+                    <h5 class="mt-3 mb-3">Nome do Jogo</h5>
+                    <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
+                </div>
+            <?php
+            endfor;
+            ?>
+        </div>
+        <div class="row mt-5">
+            <div class="col-12 text-center">
+                <h2>Entre em contato</h2>
             </div>
-
-            <div class="col-3 text-center">
-                <img src="img/jogo02.jpg" class="img-fluid">
-                <h5 class="mt-3 mb-3">Nome do Jogo</h5>
-                <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
+            <div class="col-12 text-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas deserunt nesciunt maiores similique esse molestias ea et voluptates voluptatem id magni cum error quo consequatur ratione sint magnam, provident quisquam!</p>
             </div>
-
-            <div class="col-3 text-center">
-                <img src="img/jogo03.jpg" class="img-fluid">
-                <h5 class="mt-3 mb-3">Nome do Jogo</h5>
-                <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
-            </div>
-
-            <div class="col-3 text-center">
-                <img src="img/jogo04.jpg" class="img-fluid">
-                <h5 class="mt-3 mb-3">Nome do Jogo</h5>
-                <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
-            </div>
+        </div>
+        <div class="row">
+            <form action="inserir-contato.php" method="post">
+                <div class="col">
+                    <div class="mb-2">
+                        <input type="text" class="form-control" name="nome" placeholder="Nome">
+                    </div>
+                    <div class="mb-2">
+                        <input type="tel" class="form-control" name="telefone" placeholder="Telefone">
+                    </div>
+                    <div class="mb-2">
+                        <textarea class="form-control" name="duvida" placeholder="Dúvida" rows="4"></textarea>
+                    </div>
+                    <div class="mb-2 text-center">
+                        <button type="submit" class="btn btn-success w-25">ENVIAR</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
-</body>
+    </body>
 
-</html>
+    </html>
