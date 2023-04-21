30410a0b29bac076b10a20d467fb0e613db6c2e4
Guto Xavier <gutoffline@gmail.com>
2023-04-20 21:23:27 -0300

    catalogo de jogos - jogos em destaque

--- projeto/site/index.php
+++ projeto/site/index.php
@@ -27,6 +27,37 @@
         </div>
     </div>
 
+    <div class="container mt-5 mb-3">
+        <div class="row">
+            <div class="col-12 text-center"><h1>Jogos em destaque</h1></div>
+            <div class="col-12 text-center"><p>Aqui você encontrará os jogos mais incríveis e envolventes do momento, cuidadosamente selecionados para a sua diversão. Desafie suas habilidades, experimente novos mundos e personagens, e divirta-se como nunca antes. Com gráficos de tirar o fôlego e jogabilidade viciante, nossa seleção de jogos em destaque é perfeita para os gamers mais exigentes. Venha conferir e descubra o seu próximo jogo favorito!</p></div>
+        </div>
+        <div class="row">
+            <div class="col-3 text-center">
+                <img src="img/jogo01.jpg" class="img-fluid">
+                <h5 class="mt-3 mb-3">Nome do Jogo</h5>
+                <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
+            </div>
+
+            <div class="col-3 text-center">
+                <img src="img/jogo02.jpg" class="img-fluid">
+                <h5 class="mt-3 mb-3">Nome do Jogo</h5>
+                <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
+            </div>
+
+            <div class="col-3 text-center">
+                <img src="img/jogo03.jpg" class="img-fluid">
+                <h5 class="mt-3 mb-3">Nome do Jogo</h5>
+                <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
+            </div>
+
+            <div class="col-3 text-center">
+                <img src="img/jogo04.jpg" class="img-fluid">
+                <h5 class="mt-3 mb-3">Nome do Jogo</h5>
+                <a href="nomedojogo.php" class="btn btn-outline-primary">VER MAIS</a>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
