e4f91f1f35903781c2106ecd6a9ebfefbd9d1385
Guto Xavier <gutoffline@gmail.com>
2023-05-04 22:24:33 -0300

    excluir jogo

--- projeto/site/listar-jogos.php
+++ projeto/site/listar-jogos.php
@@ -26,14 +26,18 @@ include "menu-sistema.php";
                     $sql_buscar = "select * from jogo";
                     $todos_os_jogos = mysqli_query($conexao, $sql_buscar);
                     while ($um_jogo = mysqli_fetch_assoc($todos_os_jogos)) :
-                        ?>
+                    ?>
                         <tr>
                             <td> <?php echo $um_jogo["id"]; ?></td>
                             <td> <?php echo $um_jogo["titulo"]; ?> </td>
                             <td> <?php echo $um_jogo["categoria"]; ?> </td>
-                            <td>Excluir jogo</td>
+                            <td>
+                                <a href="excluir-jogos.php?id=<?php echo $um_jogo["id"]; ?>">
+                                    <img src="img/icone-excluir.png" width="20">
+                                </a>
+                            </td>
                         </tr>
-                        <?php
+                    <?php
                     endwhile;
                     mysqli_close($conexao);
                     ?>
@@ -41,10 +45,35 @@ include "menu-sistema.php";
             </table>
         </div>
     </div>
+    <?php
+
+$msg = $_GET['msg'] ?? "";
+
+if ($msg == "excluido") {
+?>
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div id="liveToast" class="toast bg-danger" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <img src="img/icone-excluir.png" width="24" class="rounded me-2" alt="...">
+                <strong class="me-auto">ATENÇÃO</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body text-white">
+                Jogo excluído com sucesso!
+            </div>
+        </div>
+    </div>
+<?php
+}
+?>
 </div>
 
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script>
+const toastLiveExample = document.getElementById('liveToast')
+const toast = new bootstrap.Toast(toastLiveExample)
+toast.show()
+</script>
 
 </body>
 
